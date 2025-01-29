8d399530a9fb7452a5b5a7dede648f611e97a0b8
Dharshini <dharshini.s2023aiml@sece.ac.in>
2025-01-29 10:55:14 +0530

    Add files via upload

---
+++ Notes/Day 10-14/Node App/src/calculator.js
@@ -0,0 +1,6 @@
+const num1 = 10
+const num2 = 20
+const calc = () =>{
+    console.log(num1+num2)
+}
+module.exports = calc;
---
+++ Notes/Day 10-14/Node App/src/greeting.js
@@ -0,0 +1,7 @@
+
+const greetingMessage = 'Hello , from the custom module'
+const greet  = () => {
+  console.log(greetingMessage)
+}
+
+module.exports = greet
